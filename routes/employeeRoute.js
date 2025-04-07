e5aed372d00d1351b0e1be8a4672cbee928c8bef
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-04-07 14:42:55 -0500

    Delete is working, but commented out

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { create, fetch, update } from '../controller/employeeController.js'
+import { create, fetch, update, /*  deleteEmployee  */ } from '../controller/employeeController.js'
 const route = express.Router()
 
 route.post('/create', create)
 route.get('/fetch', fetch)
 route.put('/update/:id', update)
+//route.delete('/delete/:id', deleteEmployee)
 
 export default route
