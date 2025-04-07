5bffbfec5a204b49acec63069a2ac059f81479d1
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-04-07 13:23:39 -0500

    Create function working

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,3 +1,23 @@
+import Employee from "../model/employeeModel.js"
+
+export const create = async(req, res) => {
+    try {
+        const employeeData = new Employee(req.body)
+        const { userName } = employeeData
+        const employeeExist = await Employee.findOne({ userName })
+        
+        if (employeeExist) {
+            return res.status(200).json({ message: "User already exists" })
+        }
+
+        const savedEmployee = await employeeData.save()
+        res.status(201).json(savedEmployee)
+
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" })
+    }
+}
+
 export const fetch = async (req, res) => {
     try {
         res.json("Hellow World!")
