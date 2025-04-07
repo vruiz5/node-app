c9bc10ffbfd752b3aeb10b1d3dbd07197da75980
Victor Ruiz <vruiz5@stu.southtexascollege.edu>
2025-04-07 14:50:52 -0500

    finding one employee is working

--- routes/employeeRoute.js
+++ routes/employeeRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { create, fetch, update, /*  deleteEmployee  */ } from '../controller/employeeController.js'
+import { create, fetch, update, /*  deleteEmployee,  */  findEmployee } from '../controller/employeeController.js'
 const route = express.Router()
 
 route.post('/create', create)
 route.get('/fetch', fetch)
 route.put('/update/:id', update)
 //route.delete('/delete/:id', deleteEmployee)
+route.get('/fetch/:id', findEmployee)
 
 export default route
